11b6440cc449abe208ee2334670d830e028de51b
JKiskisl <smicscl@gmail.com>
2022-05-18 18:41:32 +0300

    init

---
+++ ketvpraktNoforms/AESCrypto.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ketvpraktNoforms
+{
+    class AESCrypto
+    {
+        ICryptoTransform encryptor;
+        ICryptoTransform decryptor;
+
+        /// <summary>
+        /// Initialize AES class object and Cipher Object
+        /// </summary>
+        /// <param name="sKey">Secret key must be 8/16/32 long</param>
+        public AESCrypto(string sKey)
+        {
+            RijndaelManaged aes = new RijndaelManaged();
+
+            byte[] key = Encoding.UTF8.GetBytes(sKey);
+            byte[] IV = Encoding.UTF8.GetBytes(sKey);
+
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Mode = CipherMode.CBC;
+            aes.KeySize = 128;
+            aes.BlockSize = 128;
+            decryptor = aes.CreateDecryptor(key, IV);
+            encryptor = aes.CreateEncryptor(key, IV);
+        }
+
+        /// <summary>
+        /// Encrypt File
+        /// </summary>
+        /// <param name="inputFile">Input file path</param>
+        /// <param name="outputFile">Output file path</param>
+        public void EncryptFile(string inputFile, string outputFile)
+        {
+            using (FileStream fsCrypt = new FileStream(outputFile, FileMode.Create))
+            {
+
+                using (CryptoStream cs = new CryptoStream(fsCrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (FileStream fsIn = new FileStream(inputFile, FileMode.Open))
+                    {
+                        int data;
+                        while ((data = fsIn.ReadByte()) != -1)
+                        {
+                            cs.WriteByte((byte)data);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Decrypt File
+        /// </summary>
+        /// <param name="inputFile">Input file path</param>
+        /// <param name="outputFile">Output File path</param>
+        public void DecryptFile(string inputFile, string outputFile)
+        {
+            using (FileStream fsCrypt = new FileStream(inputFile, FileMode.Open))
+            {
+                using (FileStream fsOut = new FileStream(outputFile, FileMode.Create))
+                {
+                    using (CryptoStream cs = new CryptoStream(fsCrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        int data; while ((data = cs.ReadByte()) != -1)
+                        { fsOut.WriteByte((byte)data); }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Encrypt text
+        /// </summary>
+        /// <param name="input">Input text</param>
+        /// <returns></returns>
+        public string EncryptText(string input)
+        {
+            string output = "";
+            using (Stream fsCrypt = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(fsCrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    var bytes = Encoding.UTF8.GetBytes(input);
+                    cs.Write(bytes, 0, bytes.Length);
+                    cs.FlushFinalBlock();
+                    fsCrypt.Position = 0;
+                    byte[] bytes1 = new byte[fsCrypt.Length];
+                    fsCrypt.Read(bytes1, 0, (int)fsCrypt.Length);
+                    output = Convert.ToBase64String(bytes1);
+                }
+            }
+            return output;
+        }
+
+        /// <summary>
+        /// Decrypt Text
+        /// </summary>
+        /// <param name="input">input text</param>
+        /// <returns></returns>
+        public string DecryptText(string input)
+        {
+            string output = "";
+            using (Stream fsCrypt = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(fsCrypt, decryptor, CryptoStreamMode.Write))
+                {
+                    var bytes = Convert.FromBase64String(input);
+                    cs.Write(bytes, 0, bytes.Length);
+                    cs.FlushFinalBlock();
+
+                    fsCrypt.Position = 0;
+                    StreamReader reader = new StreamReader(fsCrypt);
+                    output = reader.ReadToEnd();
+                }
+            }
+            return output;
+        }
+        //-----------------------
+        private static byte[] encrypt_aes256(byte[] unencrypted, byte[] key, byte[] salt)
+        {
+            byte[] encrypted = null;
+            using (MemoryStream memory_stream = new MemoryStream())
+            {
+                using (RijndaelManaged aes256 = new RijndaelManaged())
+                {
+                    aes256.KeySize = 256; //64 chars equals aes256 key size
+                    aes256.BlockSize = 128;//block = algorithm(256)/2
+                    Rfc2898DeriveBytes rfc_key = new Rfc2898DeriveBytes(key, salt, 2000);
+                    aes256.Key = rfc_key.GetBytes(aes256.KeySize / 8);
+                    aes256.IV = rfc_key.GetBytes(aes256.BlockSize / 8);
+                    aes256.Mode = CipherMode.CBC;//encrypts into blocks
+                    using (CryptoStream crypto_stream = new CryptoStream(memory_stream, aes256.CreateEncryptor(), CryptoStreamMode.Write)) //creates the encryptor
+                    {
+                        crypto_stream.Write(unencrypted, 0, unencrypted.Length); //writes encrypted bytes to a memory stream
+                        crypto_stream.Close();
+                    }
+                    encrypted = memory_stream.ToArray();
+                }
+            }
+            return encrypted;
+        }
+        private static byte[] decrypt_aes256(byte[] encrypted, byte[] key, byte[] salt)
+        {
+            byte[] unencrypted = null;
+            using (MemoryStream memory_stream = new MemoryStream())
+            {
+                using (RijndaelManaged aes256 = new RijndaelManaged())
+                {
+                    aes256.KeySize = 256; //64 chars equals aes256 key size
+                    aes256.BlockSize = 128;//block = algorithm(256)/2
+                    Rfc2898DeriveBytes rfc_key = new Rfc2898DeriveBytes(key, salt, 2000);
+                    aes256.Key = rfc_key.GetBytes(aes256.KeySize / 8);
+                    aes256.IV = rfc_key.GetBytes(aes256.BlockSize / 8);
+                    aes256.Mode = CipherMode.CBC;//encrypts into blocks
+                    using (CryptoStream crypto_stream = new CryptoStream(memory_stream, aes256.CreateDecryptor(), CryptoStreamMode.Write)) //creates the decryptor
+                    {
+                        crypto_stream.Write(encrypted, 0, encrypted.Length); //writes decrypted bytes to a memory stream
+                        crypto_stream.Close();
+                    }
+                    unencrypted = memory_stream.ToArray();
+                }
+            }
+            return unencrypted;
+        }
+
+        public static void Encrypt(string file, string key, string salt)
+        {
+            byte[] unencrypted = File.ReadAllBytes(file);
+            File.Delete(file);
+            byte[] key_bytes = Encoding.UTF8.GetBytes(key);
+            key_bytes = SHA256Managed.Create().ComputeHash(key_bytes);
+            byte[] salt_bytes = Encoding.UTF8.GetBytes(salt);
+            salt_bytes = SHA256Managed.Create().ComputeHash(salt_bytes);
+            byte[] encrypted = encrypt_aes256(unencrypted, key_bytes, salt_bytes);
+            File.WriteAllBytes(file, encrypted);
+        }
+        public static void Decrypt(string file, string key, string salt)
+        {
+            byte[] encrypted = File.ReadAllBytes(file);
+            File.Delete(file);
+            byte[] key_bytes = Encoding.UTF8.GetBytes(key);
+            key_bytes = SHA256Managed.Create().ComputeHash(key_bytes);
+            byte[] salt_bytes = Encoding.UTF8.GetBytes(salt);
+            salt_bytes = SHA256Managed.Create().ComputeHash(salt_bytes);
+            byte[] decrypted = decrypt_aes256(encrypted, key_bytes, salt_bytes);
+            File.WriteAllBytes(file, decrypted);
+        }
+
+
+
+
+    }
+}
---
+++ ketvpraktNoforms/Program.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ketvpraktNoforms
+
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+
+            String KEY = "SecretKey1234567";
+            String TestPath = "C:\\Users\\smics\\Desktop\\InfosaugumasNoFORMS\\ketvpraktNoforms\\Test.txt";
+            AESCrypto aes = new AESCrypto(KEY);
+
+            AESCrypto.Decrypt(TestPath, KEY, "labas");
+            #region "Stuff"
+            Console.WriteLine("Choose an option: ");
+            Console.WriteLine("1. Create a new password");
+            Console.WriteLine("2. Search for password");
+            Console.WriteLine("3. Renew password");
+            Console.WriteLine("4. Delete password");
+
+
+            Console.WriteLine("0. to exit");
+            int option = Convert.ToInt32(Console.ReadLine());
+            while (option != 0)
+            {
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Create new password");
+                        Console.WriteLine("---------------");
+                        Console.WriteLine("Password name:");
+                        String passwordName = Console.ReadLine();
+                        Console.WriteLine("Password:");
+                        String password = Console.ReadLine();
+                        Console.WriteLine("URL:");
+                        String URL = Console.ReadLine();
+                        Console.WriteLine("Comment");
+                        String Comment = Console.ReadLine();
+                        String EncryptedPassword = aes.EncryptText(password);
+
+                        using (StreamWriter writer = new StreamWriter(Path.Combine(TestPath), true))
+                        {
+                            writer.WriteLine("Password name:" + passwordName);
+                            writer.WriteLine("Password: " + EncryptedPassword);
+                            writer.WriteLine("URL:" + URL);
+                            writer.WriteLine("Comment: " + Comment);
+                        }
+
+
+                        Console.WriteLine("Password saved!");
+                        break;
+                    case 2:
+                        Console.WriteLine("Search password name");
+                        String search = Console.ReadLine();
+
+                        string[] words = File.ReadAllText(TestPath).Split(':');
+                        bool condition = false;
+                        for (int i = 0; i < words.Length; i++)
+                        {
+                            if (words[i].Contains(search) == true)
+                            {
+                                condition = true;
+                                break;
+                            }
+                            else
+                            {
+                                condition = false;
+                            }
+                        }
+                        if (condition == true)
+                        {
+                            Console.WriteLine("{0} found in file", search);
+
+                            string[] lines = File.ReadAllLines(TestPath);
+                            for (int i = 0; i < lines.Length; i++)
+                            {
+                                var line = lines[i];
+                                if (line.Contains(search))
+                                {
+                                    Console.WriteLine(lines[i++]);
+                                    Console.WriteLine(lines[i++]);
+                                    Console.WriteLine(lines[i++]);
+                                    Console.WriteLine(lines[i++]);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("not found");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Renew password");
+                        String choice = Console.ReadLine();
+
+                        string[] wordss = File.ReadAllText(TestPath).Split(' ');
+                        bool conditionn = false;
+                        for (int i = 0; i < wordss.Length; i++)
+                        {
+                            if (wordss[i].Contains(choice) == true)
+                            {
+                                conditionn = true;
+                                break;
+                            }
+                            else
+                            {
+                                conditionn = false;
+                            }
+                        }
+                        if (conditionn == true)
+                        {
+                            Console.WriteLine("{0} found in file", choice);
+                            string[] lines = File.ReadAllLines(TestPath);
+                            for (int i = 0; i < lines.Length; i++)
+                            {
+                                var line = lines[i];
+                                if (line.Contains(choice))
+                                {
+                                    Console.WriteLine("Password name: ");
+                                    String tempName = Console.ReadLine();
+                                    lines[i++] = "Password name: " + tempName;
+                                    File.WriteAllLines(TestPath, lines);
+
+                                    Console.WriteLine("Password: ");
+                                    String tempPw = Console.ReadLine();
+                                    tempPw = aes.EncryptText(tempPw);
+                                    lines[i++] = "Password: " + tempPw;
+                                    File.WriteAllLines(TestPath, lines);
+
+                                    Console.WriteLine("URL: ");
+                                    String tempURL = Console.ReadLine();
+                                    lines[i++] = "URL: " + tempURL;
+                                    File.WriteAllLines(TestPath, lines);
+
+                                    Console.WriteLine("Comment: ");
+                                    String tempComm = Console.ReadLine();
+                                    lines[i++] = "Comment: " + tempComm;
+                                    File.WriteAllLines(TestPath, lines);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not found");
+                        }
+
+                        break;
+                    case 4:
+                        Console.WriteLine("Password deletion");
+                        String tempsearch = Console.ReadLine();
+
+                        string[] tempwords = File.ReadAllText(TestPath).Split(' ');
+                        bool tempcondition = false;
+                        for (int i = 0; i < tempwords.Length; i++)
+                        {
+                            if (tempwords[i].Contains(tempsearch) == true)
+                            {
+                                tempcondition = true;
+                                break;
+                            }
+                            else
+                            {
+                                tempcondition = false;
+                            }
+                        }
+                        if (tempcondition == true)
+                        {
+                            Console.WriteLine("{0} found in file", tempsearch);
+
+                            Console.WriteLine("Are you sure about deleting this password?");
+                            Console.WriteLine("Y/N");
+                            String answer = Console.ReadLine();
+                            if (answer == "Y")
+                            {
+                                string[] lines = File.ReadAllLines(TestPath);
+                                for (int i = 0; i < lines.Length; i++)
+                                {
+                                    var line = lines[i];
+                                    if (line.Contains(tempsearch))
+                                    {
+                                        //delete lines
+                                        lines[i++] = "";
+                                        File.WriteAllLines(TestPath, lines);
+                                        lines[i++] = "";
+                                        File.WriteAllLines(TestPath, lines);
+                                        lines[i++] = "";
+                                        File.WriteAllLines(TestPath, lines);
+                                        lines[i++] = "";
+                                        File.WriteAllLines(TestPath, lines);
+                                    }
+                                }
+                                var removeWhite = File.ReadAllLines(TestPath).Where(arg => !string.IsNullOrWhiteSpace(arg));
+                                File.WriteAllLines(TestPath, removeWhite);
+                                Console.WriteLine("Succesfully deleted!");
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("not found");
+                        }
+                        break;
+                    case 9:
+
+                        Console.WriteLine("File Encrypted.");
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine("Choose an option: ");
+                option = Convert.ToInt32(Console.ReadLine());
+            }
+            #endregion
+
+            AESCrypto.Encrypt(TestPath, KEY, "labas");
+        }
+    }
+
+
+}
